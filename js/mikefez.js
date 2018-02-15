59643b7e73106696e31d0949762d2df9c17144d6
Jeff Cole <coleti16@students.ecu.edu>
2018-02-15 07:12:19 -0500

    Remove duplicated directory

--- js/mikefez.js
+++
@@ -1,9 +0,0 @@
-
-jQuery(document).ready(function(){
-	// $('.menu-item').addClass('menu-trigger');
-	$('.menu-trigger').click(function(){
-		// $('#menu-trigger').toggleClass('clicked');
-		$('.container').toggleClass('push');
-		$('.menu-type').toggleClass('open');
-	});
-});
